437417191cb584569c964c04bc90bd96da116ad7
ksteimel <ksteimel@iu.edu>
2019-06-04 16:39:35 -0400

    Adding tests for Earley parser, removed tests for CYk

--- test.jl
+++ test.jl
@@ -6,7 +6,7 @@ include("src/CFG.jl")
 			D : dog
 			"""
 	productions, lexicon = CFG.read_rules(simple_rules)
-	res_productions = Dict(("D", "N") => "NP")
+	res_productions = Dict("NP" => [("D", "N")])
 	res_lexicon = Dict("dog" => ["D"])
 	@test res_productions == productions
 	@test res_lexicon == lexicon
@@ -15,7 +15,7 @@ include("src/CFG.jl")
 				D : {dog, cat, mouse}
 				"""
 	productions, lexicon = CFG.read_rules(multi_part_lexicon)
-	res_productions = Dict(("Q", "N") => "NP")
+	res_productions = Dict("NP" => [("Q", "N")])
 	res_lexicon = Dict("dog" => ["D"], "cat" => ["D"], "mouse" => ["D"])
 	@test res_lexicon == lexicon
 	@test res_productions == productions
@@ -26,32 +26,72 @@ include("src/CFG.jl")
 	res_lexicon = Dict("dog" => ["D","V"])
 	productions, lexicon = CFG.read_rules(ambiguous_lexicon)
 	@test lexicon == res_lexicon
+	ambiguous_production = """
+                            NP -> D N
+                            NP -> Adj N
+                            """
+    productions, lexicon = CFG.read_rules(ambiguous_production)
+    res_productions = Dict("NP" => [("D","N"), ("Adj","N")])
+    @test productions == res_productions
 end
-@testset "binarization" begin
-	@testset "binary_rules_are_unmodified" begin
-		# check that binary rules pass through unadulterated
-		binary_prod = Dict(("D","N") => "NP")
-		lexicon = Dict("the" => ["D"], "dog" => ["N"])
-		res_productions, res_lex, pairings = CFG.binarize!(binary_prod, lexicon)
-		@test res_productions == binary_prod
-	end
-	@testset "unary_productions" begin
-		# check that unary productions are fixed on the production side
-		unary_prod = Dict(("D", "N") => "NP", ("NP",) => "S")
-		lexicon = Dict("the" => ["D"], "dog" => ["N"])
-		target_prod = Dict(("D", "N") => "S")
-		res_prod, res_lex, pairings = CFG.binarize!(unary_prod, lexicon)	
-		@test target_prod == res_prod
-	
-		# check that modifications to the lexicon in order to fix productions
-		# are done appropriately
-		unary_prod = Dict(("D",) => "NP")
-		lexicon = Dict("the" => ["D"])
-		res_prod, res_lex, pairings = CFG.binarize!(unary_prod, lexicon)
-		target_lex = Dict("the" => ["NP"])
-		@test target_lex == res_lex
-	end
+
+@testset "rule_verify" begin
+    productions = Dict("NP" => ["D","N"], "VP" => ["V", "NP"])
+    lexicon = Dict("dog" => ["N", "V"], "the" => ["D"])
+    @test CFG.verify_system(productions, lexicon) == true
+    productions = Dict("NP" => ["D","N"], "VP" => ["V", "NP"])
+    lexicon = Dict("dog" => ["N", "V"])
+    @test CFG.verify_system(productions, lexicon) == false
+    
 end
+
+@testset "earley_pieces" begin
+    lexicon = Dict("dog" => ["V","N"], "cat" => ["N", "Adj"])
+    parts_of_speech = ["V","N","Adj"]
+    @test sort(parts_of_speech) == sort(unique(collect(Iterators.flatten(values(lexicon)))))
+    @testset "EarleyState" begin
+        @testset "ordered_constructor" begin
+            sample_state = CFG.EarleyState(1,2,3,["NP","VP"], "S", 1, 3)
+            @test sample_state.state_num == 1
+            @test sample_state.start_index == 2
+            @test sample_state.end_index == 3
+            @test sample_state.right_hand == ["NP","VP"]
+            @test sample_state.left_hand == "S"
+            @test sample_state.dot_index == 1
+            @test sample_state.originating_state_index == 3
+        end
+        @testset "malformed_construction" begin
+            @test_throws BoundsError CFG.EarleyState(1,2,3,["NP","VP"], "S", 4, 3)
+            @test_throws BoundsError CFG.EarleyState(1,2,3, ["NP","VP"], "S", 0, 3)
+            @test CFG.EarleyState(1,2,3,["NP","VP"], "S", 3, 3).dot_index == 3
+        end
+        @testset "state_utils" begin
+            initial_state = CFG.EarleyState(1,1, 1, ["S"], "γ", 1, 0)
+            @test CFG.next_cat(initial_state) == "S"
+            initial_state = CFG.EarleyState(1,1, 1, ["S"], "γ", 1, 0)
+            @test CFG.is_incomplete(initial_state) == true
+            complete_state = CFG.EarleyState(1,1, 1, ["S"], "γ", 2, 0)
+            @test CFG.is_incomplete(complete_state) == false
+        end
+    end
+    @testset "predictor" begin
+        chart = CFG.EarleyState[]
+        initial_state = CFG.EarleyState(1,1, 1, ["S"], "γ", 1, 0)
+        push!(chart, initial_state)
+        lexicon = Dict("dog" => ["N","V"], "cat" => ["N","Adj"])
+        productions = Dict("S" => [["NP","VP"]], 
+                            "NP" => [["N"]],
+                            "VP" => [["V"]])
+        step_index = 1
+        charts = [chart]
+        CFG.predictor!(charts, step_index, productions, lexicon, initial_state)
+        @test length(charts[1]) == 2
+        state_res = CFG.EarleyState(2, 1, 1, ["NP","VP"], "S", 1, 1)
+        @test charts[1][2] == state_res
+    end
+end
+
+println()
 tokens = ["the","dog","runs","quite","fast"]
 non_terminals = ["NP","VP","Av","Aj","D","N", "V", "AP", "S"]
 lattice = zeros(Bool, 5, 5, length(non_terminals))
