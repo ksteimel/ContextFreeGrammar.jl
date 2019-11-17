6049bf80c39aa048cfc10c0f3594ecffcacfd097
ksteimel <ksteimel@iu.edu>
2019-11-17 12:22:21 -0500

    Added fixtures showing how to do parsing and generation

---
+++ test/fixtures/parse.jl
@@ -0,0 +1,19 @@
+using CFG
+rule_file = open("../doc/rules.txt")
+rule_text = readlines(rule_file)
+rule_text = join(rule_text, "\n")
+productions, lexicon = CFG.read_rules(rule_text)
+in_sents_file = open("./gen_sents.txt")
+sents = readlines(in_sents_file)
+function parse_all(productions, lexicon, sents)
+	sent = [sent for sent in sents if strip(sent) != ""]
+	Threads.@threads for sent in sents
+		sent = split(sent, " ")
+                sent = [string(strip(word)) for word in sent if strip(word) != ""]
+		CFG.parse_earley(productions, lexicon, sent, debug=false)		
+	end
+end
+println("Problem sizes ")
+println(sum([length(productions[kii]) for kii in keys(productions)]))
+println(sum([length(lexicon[kii]) for kii in keys(lexicon)]))
+@time parse_all(productions, lexicon, sents[1:300])
