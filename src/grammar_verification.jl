14ca9b6d879626b464f95ff73a165da6069748c0
ksteimel <ksteimel@iu.edu>
2019-12-04 22:00:03 -0500

    Added check to identify pieces that occur on the rhs of a rule but no lhs

--- src/grammar_verification.jl
+++ src/grammar_verification.jl
@@ -25,7 +25,29 @@ function verify_productions(productions, lexicon)::Bool
     end
     return true
 end
-
+"""
+This works essentially the same as `verify_productions`
+except that this one returns the array of mismatched pieces
+not just a boolean value.
+"""
+function identify_missing_left_hand_sides(productions, lexicon)::Bool
+    prod_items = collect(Iterators.flatten(values(productions)))
+    prod_items = unique(prod_items)
+    lex_items = collect(Iterators.flatten(values(lexicon)))
+    lex_items = unique(lex_items)
+    missing_lhs = []
+    for options in prod_items
+        for piece in options
+            if !haskey(productions, piece) && !(piece in lex_items)
+                push!(missing_lhs, piece)
+            end
+        end
+    end
+    return missing_lhs
+end
+"""
+Checks to see if all the words in the sentence exist in the lexicon
+"""
 function verify_lexicon(lexicon, sentence)::Bool
     for word in sentence
         if !haskey(lexicon, word)
